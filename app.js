f97b11730a4e716f98abaad4d200a1994e65e384
ather ali <a.ngaming102734@gmail.com>
2025-12-31 18:01:57 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,51 @@
+// var obje1 = {
+//     stuName : 'Ather',
+//     rollNum:12,
+//     class:10
+// }
+
+// document.write(obje1.rollNum, obje1.class)
+
+// // document.write(obje1)
+// console.log(obje1)
+
+// for(var key in obje1){
+//     document.write(key +' ' + obje1[key]+"<br>")
+// }
+
+// var name 
+
+//  var obje1 = {
+//     stuName : 'Ather',
+//     rollNum:12,
+//     class:10
+// }
+
+// var newstd = 'ali'
+
+// obje1.s
+
+// var object = {
+//   : 'object',
+//     'Plan1',
+//     'Plan2 ,
+// }
+
+// for(var key in object){
+//     document.write(+''+Object[key])
+// }
+
+// var obj ={
+//     obt:'object',
+//     ObName:('Plan1'),
+//     objMon:('Plan2'),
+// }
+// // console.log(obj);
+// for(var key in obj){
+//     document.write(obj[key]+' ')
+// }
+
+// var obj={
+//     Plan1:Plan1,
+//     Prise:()
+// }
